e9113734ec90085e40e2b2789be88353132f00d7
Augustin Tacquet <augu.tacquet@gmail.com>
2023-07-21 14:54:00 +0200

    added error 404 page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-//import ErrorCSS from "./Error.module.css";
+import ErrorCSS from "./Error.module.css";
+import { Link } from "react-router-dom";
 
 function Error() {
-  return <div>404</div>;
+  return (
+    <main className={ErrorCSS.error_global_styling}>
+      <p className={ErrorCSS.error_message_http_code}>404</p>
+      <p className={ErrorCSS.error_message_text}>
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link className={ErrorCSS.return_home_link} to={"/"}>
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
+  );
 }
 
 export default Error;
